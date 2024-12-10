7be237a0e52d0f054186cc0c14f33dd6a439f321
andikadeniardi <andika.anggar@gmail.com>
2024-12-10 23:22:46 +0700

    feat: home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,27 @@
+// pages/index.tsx
+
+import Navbar from "../../../components/molecules/Navbar";
+
+const Home = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+
+      <main className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold text-center text-blue-600">
+          Welcome to My Website
+        </h1>
+        <p className="text-lg text-gray-700 mt-4">
+          This is the content of the page. Tailwind CSS should be applied
+          correctly.
+        </p>
+      </main>
+
+      <footer className="bg-blue-600 text-white text-center p-4">
+        <p>&copy; 2024 My Website</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
