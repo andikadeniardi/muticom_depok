70bc26db2373c2027cc8c96165ccebe9ec94f8bc
andikadeniardi <andika.anggar@gmail.com>
2024-12-24 08:24:59 +0700

    feat: update komputer page

--- src/app/PaketKomputer/page.tsx
+++ src/app/PaketKomputer/page.tsx
@@ -25,83 +25,92 @@ const PaketKomputer = () => {
           <p className="text-lg text-gray-600 mt-4">
             Pilih paket pelatihan komputer yang sesuai dengan kebutuhan Anda.
             Kami menawarkan berbagai paket untuk mengembangkan keterampilan di
-            bidang teknologi informasi.
+            bidang teknologi informasi yang bisa disesuaikan dengan tingkat
+            kemampuan Anda.
           </p>
         </section>
 
         {/* Section Paket Pelatihan Komputer */}
         <section className="mt-8">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
             {/* Paket Pelatihan 1 */}
             <PaketCard
-              title="Paket Dasar Komputer"
-              description="Pelatihan untuk pemula yang mencakup dasar-dasar penggunaan komputer, sistem operasi, dan aplikasi perkantoran."
-              imageUrl="/images/basic-computer.jpg"
-              price="Rp 1.000.000"
-              link="/paket/dasar-komputer"
+              title="Introduction to Computer (Tingkat Dasar/Pemula) Start Usia 5th"
+              description="Pelatihan untuk mengenal komputer dan sistem operasi, serta aplikasi perkantoran dasar seperti Word dan Excel. Cocok untuk pemula atau anak-anak usia 5 tahun ke atas yang ingin belajar menggunakan komputer untuk kegiatan sehari-hari."
+              imageUrl="/paket1.jpeg"
+              price="Rp 800.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Pelatihan%20Dasar%20Komputer"
+              duration="8 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 2 */}
             <PaketCard
-              title="Paket Microsoft Office"
-              description="Pelatihan mendalam mengenai penggunaan Microsoft Word, Excel, PowerPoint, dan Outlook."
-              imageUrl="/images/microsoft-office.jpg"
-              price="Rp 1.200.000"
-              link="/paket/microsoft-office"
+              title="Paket Administrasi Perkantoran (Tingkat Menengah)"
+              description="Pelatihan untuk pemula dalam menggunakan Microsoft Word, Excel, dan PowerPoint. Paket ini cocok bagi Anda yang baru mulai bekerja dengan aplikasi perkantoran dan ingin meningkatkan keterampilan dasar dalam mengelola dokumen, data, dan presentasi."
+              imageUrl="/paket2.jpeg"
+              price="Rp 1.600.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Administrasi%20Perkantoran%20Tingkat%20Menengah"
+              duration="16 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 3 */}
             <PaketCard
-              title="Paket Desain Grafis"
-              description="Pelatihan untuk menjadi ahli desain grafis menggunakan software seperti Adobe Photoshop dan Illustrator."
-              imageUrl="/images/graphic-design.jpg"
-              price="Rp 2.500.000"
-              link="/paket/desain-grafis"
+              title="Paket Administrasi Perkantoran (Tingkat Lanjutan)"
+              description="Meningkatkan keterampilan dalam Microsoft Word, Excel, PowerPoint (intermediate), serta Outlook dan MS Teams. Peserta juga akan mempelajari penggunaan browser dan internet untuk kebutuhan administrasi yang lebih kompleks dan kolaboratif di lingkungan kerja."
+              imageUrl="/paket3.jpeg"
+              price="Rp 2.000.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Administrasi%20Perkantoran%20Tingkat%20Lanjutan"
+              duration="16 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 4 */}
             <PaketCard
-              title="Paket Web Development"
-              description="Pelatihan untuk membangun website menggunakan HTML, CSS, JavaScript, dan tools web development lainnya."
-              imageUrl="/images/web-development.jpg"
-              price="Rp 3.000.000"
-              link="/paket/web-development"
+              title="Paket Data Analyst (Tingkat Mahir)"
+              description="Pelatihan untuk menjadi Data Analyst profesional dengan fokus pada penggunaan Excel Power Query dan Power Pivot (Dashboard) untuk mengolah data. Anda juga akan mempelajari pembuatan visualisasi data menggunakan Ms.Power BI, alat analisis data yang dibutuhkan di industri."
+              imageUrl="/paket4.jpeg"
+              price="Rp 3.500.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Data%20Analyst"
+              duration="16 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 5 */}
             <PaketCard
-              title="Paket Jaringan Komputer"
-              description="Pelatihan untuk memahami konsep jaringan komputer, setting jaringan, dan troubleshooting."
-              imageUrl="/images/networking.jpg"
-              price="Rp 2.200.000"
-              link="/paket/jaringan-komputer"
+              title="Paket Design Grafis Illustrator & Photoshop"
+              description="Pelatihan intensif mengenai desain grafis menggunakan Adobe Illustrator dan Photoshop. Didesain untuk membantu Anda mempelajari teknik desain visual untuk pembuatan logo, brosur, poster, dan materi pemasaran lainnya."
+              imageUrl="/paket1.jpeg"
+              price="Rp 2.000.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Design%20Grafis%20Illustrator%20dan%20Photoshop"
+              duration="16 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 6 */}
             <PaketCard
-              title="Paket Sistem Operasi"
-              description="Pelatihan mengenai instalasi, konfigurasi, dan penggunaan sistem operasi Windows dan Linux."
-              imageUrl="/images/operating-system.jpg"
-              price="Rp 2.000.000"
-              link="/paket/sistem-operasi"
+              title="Paket AutoCAD 2D & 3D"
+              description="Paket pelatihan Autocad ini untuk mempelajari teknik desain dalam dunia industri. Teknik ini menggunakan AutoCAD 2D dan 3D, sangat cocok untuk mereka yang berkarir di bidang arsitektur dan desain teknik, serta yang tertarik membuat gambar teknik profesional."
+              imageUrl="/paket2.jpeg"
+              price="Rp 2.500.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20AutoCAD%202D%20dan%203D"
+              duration="16 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 7 */}
             <PaketCard
-              title="Paket Keamanan Jaringan"
-              description="Pelatihan mengenai pengamanan jaringan komputer, firewall, dan enkripsi data."
-              imageUrl="/images/network-security.jpg"
-              price="Rp 2.500.000"
-              link="/paket/keamanan-jaringan"
+              title="Paket Web Master Admin, Programming, Design"
+              description="Pelatihan komprehensif untuk mengelola dan mengembangkan website. Program ini mencakup berbagai aspek, mulai dari administrasi web, desain UI/UX, hingga pengembangan backend dan pemrograman web."
+              imageUrl="/paket3.jpeg"
+              price="Rp 2.600.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Web%20Master"
+              duration="16 x 1.5 jam"
             />
 
             {/* Paket Pelatihan 8 */}
             <PaketCard
-              title="Paket IT Support"
-              description="Pelatihan untuk menjadi teknisi IT yang mampu memberikan dukungan teknis untuk perangkat keras dan perangkat lunak."
-              imageUrl="/images/it-support.jpg"
-              price="Rp 2.800.000"
-              link="/paket/it-support"
+              title="Paket Animasi 3D Max, Adobe Premiere"
+              description="Pelatihan untuk membuat animasi 3D dan mengedit video dengan menggunakan software 3D Max dan Adobe Premiere. Program ini cocok bagi Anda yang ingin mengembangkan karir di bidang pembuatan animasi dan produksi film."
+              imageUrl="/paket4.jpeg"
+              price="Rp 2.600.000"
+              link="https://wa.me/6281316907115?text=Saya%20tertarik%20dengan%20Paket%20Animasi%203D%20Max%20dan%20Adobe%20Premiere"
+              duration="16 x 1.5 jam"
             />
           </div>
 
